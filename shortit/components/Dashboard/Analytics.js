b6f8da51db29a08f12d0ab6343fd980068055084
pr0xy1337 <milcho.kyosev@hotmail.com>
2023-03-14 11:33:36 +0200

    Create Analytics.js

---
+++ shortit/components/Dashboard/Analytics.js
@@ -0,0 +1,177 @@
+import Link from "next/link";
+import React, { useState } from 'react'
+
+export default function Analytics({ children }) {
+    const [url, setUrl] = useState('');
+    const [customSlug, setCustomSlug] = useState('');
+    const [isCustom, setIsCustom] = useState(false);
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     console.log('Submitting form...', { url, customSlug, isCustom });
+    // };
+
+    const handleCustomSlugChange = (event) => {
+        setCustomSlug(event.target.value);
+    };
+
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value);
+    };
+
+    const handleIsCustomChange = (event) => {
+        setIsCustom(event.target.checked);
+    };
+
+    const [originalUrl, setOriginalUrl] = useState("");
+    const [shortUrl, setShortUrl] = useState("http://example.com");
+    const [shortenedUrl, setShortenedUrl] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [expandedRowIndex, setExpandedRowIndex] = useState(null);
+    const [links, setLinks] = useState([{
+        id: "kljAlk4",
+        name: "Google",
+        description: "Link to google",
+        url: "http://google.com/",
+        totalVisits: 12,
+        details: "More info here"
+    }, {
+        id: "kagsAlk4",
+        name: "Yahoo",
+        description: "Link to yahoo",
+        shortUrl: "http://yahoo.com/",
+        totalVisits: 15,
+        details: "More info here"
+    }]);
+
+    const handleOriginalUrlChange = (event) => {
+        setOriginalUrl(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        setIsLoading(true);
+
+        try {
+            // const response = await axios.post("/api/shorten", { url: originalUrl });
+
+            setShortenedUrl('http://goog/asd.com');
+        } catch (error) {
+            console.error(error);
+        }
+
+        setIsLoading(false);
+    };
+
+    const copyToClipboard = async (event) => {
+        event.preventDefault();
+
+    };
+
+    return (
+        <>
+            <div className="p-4 sm:ml-64 h-screen">
+                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 my-16">
+                    <div className="flex flex-col">
+                        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table className="min-w-full divide-y divide-gray-200">
+                                        <thead className="bg-gray-50">
+                                            <tr>
+                                                <th
+                                                    scope="col"
+                                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                                >
+                                                    Name
+                                                </th>
+                                                <th
+                                                    scope="col"
+                                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                                >
+                                                    Description
+                                                </th>
+                                                <th
+                                                    scope="col"
+                                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                                >
+                                                    Short URL
+                                                </th>
+                                                <th
+                                                    scope="col"
+                                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                                >
+                                                    Total Visits
+                                                </th>
+                                                <th scope="col" className="relative px-6 py-3">
+                                                    <span className="sr-only">Edit/Delete</span>
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="bg-white divide-y divide-gray-200">
+                                            {links.map((link, index) => (
+                                                <React.Fragment key={link.id}>
+                                                    <tr className="bg-white hover:bg-gray-200">
+                                                        <td className="px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm font-medium text-gray-900">{link.name}</div>
+                                                        </td>
+                                                        <td className="px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-gray-500">{link.description}</div>
+                                                        </td>
+                                                        <td className="px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-blue-500">{link.shortUrl}</div>
+                                                        </td>
+                                                        <td className="px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-gray-500">{link.totalVisits}</div>
+                                                        </td>
+                                                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                            <button
+                                                                className="bg-green-500 hover:bg-green-600 text-white font-bold px-4 py-2 rounded"
+                                                                onClick={() => console.log(`Editing link with id ${link.id}`)}
+                                                            >
+                                                                Edit
+                                                            </button>
+                                                            <span className="mx-2 text-gray-400">|</span>
+                                                            <button
+                                                                className="bg-red-500 hover:bg-red-600 text-white font-bold px-4 py-2 rounded"
+                                                                onClick={() => console.log(`Deleting link with id ${link.id}`)}
+                                                            >
+                                                                Delete
+                                                            </button>
+                                                            <span className="mx-2 text-gray-400">|</span>
+                                                            <button
+                                                                className={`${expandedRowIndex === index
+                                                                    ? "bg-gray-500 text-white"
+                                                                    : "bg-blue-500 text-white"
+                                                                    } hover:bg-blue-600 font-bold px-4 py-2 rounded`}
+                                                                onClick={() =>
+                                                                    setExpandedRowIndex(expandedRowIndex === index ? null : index)
+                                                                }
+                                                            >
+                                                                {expandedRowIndex === index ? "Hide" : "Details"}
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                    {index === expandedRowIndex && (
+                                                        <tr>
+                                                            <td colSpan="5"> {/* makes the column spread to all 5 parent columns */}
+                                                                <div className="px-6 py-4 text-sm text-gray-500">{link.details}</div>
+                                                            </td>
+                                                        </tr>
+                                                    )}
+                                                </React.Fragment>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+
