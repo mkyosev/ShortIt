47b20fe6af35da65a82af604121d3caa8f518fa9
pr0xy1337 <milcho.kyosev@hotmail.com>
2023-03-14 11:33:44 +0200

    Create analytics.js

---
+++ shortit/pages/dashboard/analytics.js
@@ -0,0 +1,11 @@
+import Analytics from "../../components/Dashboard/Analytics"
+import DashboardLayout from "../../components/Dashboard/DashboardLayout"
+
+export default function AnalyticsPage() {
+    return (
+        <>
+            <DashboardLayout />
+            <Analytics />
+        </>
+    )
+}
