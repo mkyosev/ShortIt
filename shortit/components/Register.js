a5e0e956f90ddace6ff4a3e7d7aec08725e02a52
pr0xy1337 <milcho.kyosev@hotmail.com>
2023-03-16 17:16:21 +0200

    Update Register.js

--- shortit/components/Register.js
+++ shortit/components/Register.js
@@ -1,19 +1,42 @@
 import { useState } from "react";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repassword, setRepassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const router = new useRouter();
+
+  const handleSubmit = async (e, action) => {
     e.preventDefault();
-    // Handle register logic here
+    const { email, password } = e.target;
+
+    if (password.value != repassword) {
+      //Notification wrong pw
+      console.log('Passwords do not match ' + password.value + " | " + repassword );
+      return
+    }
+    console.log("Try + " + email.value + " " + password.value);
+    const result = await signIn('credentials', {
+      redirect: false,
+      email: email.value,
+      password: password.value,
+      action: action,
+    });
+
+    if (result.error) {
+      console.log(result.error);
+    } else {
+      router.push('/');
+    }
   };
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/2 mx-auto">
+      <form onSubmit={(e) => handleSubmit(e, 'register')} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3 mx-auto">
         <h2 className="text-2xl mb-4 font-bold text-gray-900">Register</h2>
         <div className="mb-4">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
@@ -43,14 +66,14 @@ const Register = () => {
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
-           Repeat Password
+            Repeat Password
           </label>
           <input
             className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="repassword"
             type="password"
             placeholder="Repeat Password"
-            value={password}
+            value={repassword}
             onChange={(e) => setRepassword(e.target.value)}
           />
         </div>
